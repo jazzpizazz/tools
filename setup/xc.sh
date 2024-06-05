6916f4d01deafaef3f5fb5816150040c7c699cc6
jazz <jazzpizazz@protonmail.com>
2024-06-05 11:08:36 -0400

    Add xc in a way where people still compile their own binaries

--- setup/xc.sh
+++ setup/xc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-git clone --recurse-submodules https://github.com/xct/xc.git /opt/tools/windows/ 
+git clone --recurse-submodules https://github.com/xct/xc.git /opt/tools/windows/xc/ 
 
 GO111MODULE=off go get golang.org/x/sys/...
 GO111MODULE=off go get golang.org/x/text/encoding/unicode
 GO111MODULE=off go get github.com/hashicorp/yamux
 GO111MODULE=off go get github.com/libp2p/go-reuseport
 sudo apt-get install rlwrap upx
+
+cd /opt/tools/windows/xc/ && python3 build.py
 
